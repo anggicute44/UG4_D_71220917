28833cc4b2254ff1c44e6bdd2810f172f50fb477
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2023-03-01 18:22:11 +0700

    Add files via upload

---
+++ 1_D_71220917.py
@@ -0,0 +1,10 @@
+print ("===========","BARIS ARITMATIKA","===========")
+
+def sintiiiii (mumasf,pdughq,iohglohhh):
+    haiii12 = mumasf + (iohglohhh-1) * pdughq
+    asdqwfe= iohglohhh/2*(mumasf+haiii12)
+    return asdqwfe
+mumasf=int(input("Masukkan bilangan awal : "))
+pdughq=int(input("Masukkan selisih bilangan : "))
+iohglohhh=int(input("Masukkan banyaknya suku : "))
+print (sintiiiii(mumasf,pdughq,iohglohhh))
